c4030972b1d8e1a1edbe8a51bebd5c871c88df7f
Monsoon Parajuli <parajuli.monsoon2001.com>
2024-12-20 21:03:39 +0545

    Fixed resume path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/resume.pdf"
+              href="/monsoon_resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
